a1c8841bba20bdf44a46dfbd7961eab444fbfa25
stogashi146 <stogashi146@gmail.com>
2026-01-05 22:48:24 +0900

    問題4 回答にページNoの記載

--- components.py
+++ components.py
@@ -169,9 +169,9 @@ def display_search_llm_response(llm_response):
         # ページ番号が取得できた場合のみ、ページ番号を表示（ドキュメントによっては取得できない場合がある）
         if "page" in llm_response["context"][0].metadata:
             # ページ番号を取得
-            main_page_number = llm_response["context"][0].metadata["page"]
+            main_page_number = llm_response["context"][0].metadata["page"] + 1
             # 「メインドキュメントのファイルパス」と「ページ番号」を表示
-            st.success(f"{main_file_path}", icon=icon)
+            st.success(f"{main_file_path} (ページNo.{main_page_number})", icon=icon)
         else:
             # 「メインドキュメントのファイルパス」を表示
             st.success(f"{main_file_path}", icon=icon)
@@ -204,7 +204,7 @@ def display_search_llm_response(llm_response):
             # ページ番号が取得できない場合のための分岐処理
             if "page" in document.metadata:
                 # ページ番号を取得
-                sub_page_number = document.metadata["page"]
+                sub_page_number = document.metadata["page"] + 1
                 # 「サブドキュメントのファイルパス」と「ページ番号」の辞書を作成
                 sub_choice = {"source": sub_file_path, "page_number": sub_page_number}
             else:
@@ -227,7 +227,7 @@ def display_search_llm_response(llm_response):
                 # ページ番号が取得できない場合のための分岐処理
                 if "page_number" in sub_choice:
                     # 「サブドキュメントのファイルパス」と「ページ番号」を表示
-                    st.info(f"{sub_choice['source']}", icon=icon)
+                    st.info(f"{sub_choice['source']} (ページNo.{sub_choice['page_number']})", icon=icon)
                 else:
                     # 「サブドキュメントのファイルパス」を表示
                     st.info(f"{sub_choice['source']}", icon=icon)
@@ -302,20 +302,20 @@ def display_contact_llm_response(llm_response):
             if file_path in file_path_list:
                 continue
 
+            # 参照元のありかに応じて、適したアイコンを取得
+            icon = utils.get_source_icon(file_path)
             # ページ番号が取得できた場合のみ、ページ番号を表示（ドキュメントによっては取得できない場合がある）
             if "page" in document.metadata:
                 # ページ番号を取得
-                page_number = document.metadata["page"]
+                page_number = document.metadata["page"] + 1
                 # 「ファイルパス」と「ページ番号」
                 file_info = f"{file_path}"
+                # ファイル情報を表示
+                st.info(f"{file_info} (ページNo.{page_number})", icon=icon)
             else:
                 # 「ファイルパス」のみ
                 file_info = f"{file_path}"
-
-            # 参照元のありかに応じて、適したアイコンを取得
-            icon = utils.get_source_icon(file_path)
-            # ファイル情報を表示
-            st.info(file_info, icon=icon)
+                st.info(f"{file_info}", icon=icon)
 
             # 重複チェック用に、ファイルパスをリストに順次追加
             file_path_list.append(file_path)
